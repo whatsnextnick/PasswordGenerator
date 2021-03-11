e471d48dbe01a94e3db402319ec0c9ed7bedced5
Jawascript <n.jmonplaisir@gmail.com>
2021-03-11 04:25:27 -0500

    added button function

--- script.js
+++ script.js
@@ -9,19 +9,60 @@ var numofchars = document.getElementById("numchars");
 var upperchars = document.getElementById("uppercase");
 var lowerchars = document.getElementById("lowercase");
 var numchars = document.getElementById("numeric");
-var specchars = document.getElementById("specialcharacters");
+var specchars = document.getElementById("specialchars");
 
-var generateBtn = document.querySelector("#generate");
+//Generate password
+var generater = document.getElementById('generate');
+generater.addEventListener('click', criteria);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// user input handler
+function criteria() {
+  var checkbox = document.getElementsByClassName('form-check-input');
+  var checked = false;
+  var characters = '';
 
+  // make sure user chose a criteria 
+  for (var i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      checked = true;
+    }
+  }
+
+  if (checked) {
+    (specchars.checked) ? characters += special : '';
+    (numchars.checked) ? characters += nums : '';
+    (lowerchars.checked) ? characters += lower : '';
+    (upperchars.checked) ? characters += upper : '';
+
+    //pass the password to the textarea
+    passwordText.value = createpass(characters);
+  }
 }
 
+
+  var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// };
+
+// password handler
+function createpass(characters) {
+  var password = '';
+  // default password length is 8 characters
+  var passwordLength = parseInt(numofchars.value);
+
+  // get a unique value as many times as the length set by user
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random()*characters.length);
+    password += characters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// slider label update
 var slideVal = document.getElementById("slid");
 console.log(numofchars.value);
 slideVal.innerText = numofchars.value;
@@ -32,5 +73,5 @@ numofchars.oninput = function() {
 
 console.log(typeof slideVal);
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generater.addEventListener("click", writePassword);
